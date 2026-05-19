af45a9b44352d315842ab8bb64ce3b56533ca9a5
Yang Liu <summerliu2333@gmail.com>
2026-05-19 15:07:11 -0700

    M1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
 
 using namespace std;
 
 int main() {
+    //M1
+    ifstream infile("210-final-1-SP26.txt");
+    map<string, int> airports;
+    string origin, destination;
+
+    while (infile >> origin >> destination) {
+        if (airports.find(origin) == airports.end()) airports[origin] = 0;
+        if (airports.find(destination) == airports.end()) airports[destination] = 0;
+        airports[origin]++;
+        airports[destination]++;
+    }
+    infile.close();
+ 
+    cout << "All airport traffic counts:" << endl;
+    for (auto entry : airports) {
+        cout << entry.first << ' ' << entry.second << endl;
+    }
+
     return 0;
 }
